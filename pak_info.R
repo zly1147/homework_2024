541a5413ae565d71c03ebcb3442f94101063dd8e
zly1147 <161792673+zly1147@users.noreply.github.com>
2024-03-19 09:27:29 +0800

    Add files via upload

---
+++ pak_info.R
@@ -0,0 +1,28 @@
+## ---------------------------
+## Script name: zly_pak_info.R
+## Purpose of script:获得有关tidyverse包的信息
+## Author: 张凌宇
+## Date Created: 2024-03-18
+## Copyright (c) Timothy Farewell, 2024
+## Email: zly1147@mail.ustc.edu.cn
+## ---------------------------
+## Notes:
+## ---------------------------
+
+#查找R环境中tidyverse包是否存在
+find.package("tidyverse")
+
+#如果未找到，下载并安装tidyverse
+install.packages("tidyverse")
+
+#加载tidyverse
+library(tidyverse)
+
+#通过网页了解tidyverse包的功能
+browseVignettes(package="tidyverse")
+
+#在help中搜索tidyverse相关说明页面
+help.search('^tidyverse')
+
+#获取tidyverse的help文档信息
+help(tidyverse)
